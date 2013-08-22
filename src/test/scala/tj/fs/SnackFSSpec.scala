b06a466fbd01813d1aef964c1ca8c5a0c561d2ab
Shiti <ssaxena.ece@gmail.com>
2013-08-22 10:37:16 +0530

    added tests for open() function

--- src/test/scala/tj/fs/SnackFSSpec.scala
+++ src/test/scala/tj/fs/SnackFSSpec.scala
@@ -69,6 +69,80 @@ class SnackFSSpec extends FlatSpec with BeforeAndAfterAll with MustMatchers {
     exception.getMessage must be("Can't make a directory for path %s since its a file".format(path))
   }
 
+  it should "allow to read from a file" in {
+    val fs = SnackFS(store)
+    val uri = URI.create("cfs://localhost:9000")
+    fs.initialize(uri, new Configuration())
+    val fsData = fs.create(new Path("/home/shiti/Downloads/SOMEFILE"))
+    fsData.write("SOME CONTENT".getBytes)
+    fsData.close()
+
+    val is = fs.open(new Path("/home/shiti/Downloads/SOMEFILE"))
+    var dataArray = new Array[Byte](12)
+    is.readFully(0, dataArray)
+    is.close()
+
+    val result = new String(dataArray)
+    result must be("SOME CONTENT")
+  }
+
+  it should "throw an exception when trying to open a directory" in {
+    val fs = SnackFS(store)
+    val uri = URI.create("cfs://localhost:9000")
+    fs.initialize(uri, new Configuration())
+    val path = new Path("/test")
+    fs.mkdirs(path)
+    val exception = intercept[IOException] {
+      fs.open(path)
+    }
+    exception.getMessage must be("Path %s is a directory.".format(path))
+  }
+
+  it should "throw an exception when trying to open a file which doesn't exist" in {
+    val fs = SnackFS(store)
+    val uri = URI.create("cfs://localhost:9000")
+    fs.initialize(uri, new Configuration())
+    val path = new Path("/newFile")
+    val exception = intercept[IOException] {
+      fs.open(path)
+    }
+    exception.getMessage must be("No such file.")
+  }
+
+  it should "get file status" in {
+    val fs = SnackFS(store)
+    val uri = URI.create("cfs://localhost:9000")
+    fs.initialize(uri, new Configuration())
+    val path = new Path("/home/shiti/Downloads/SOMEFILE")
+    val fsData = fs.create(path)
+    fsData.write("SOME CONTENT".getBytes)
+    fsData.close()
+
+    val status = fs.getFileStatus(path)
+    status.isFile must be(true)
+    status.getLen must be(12)
+    status.getPath must be(path)
+  }
+
+  it should "get file block locations" in {
+    val fs = SnackFS(store)
+    val uri = URI.create("cfs://localhost:9000")
+    fs.initialize(uri, new Configuration())
+    val path = new Path("/home/shiti/Downloads/SOMEFILE")
+    val fsData = fs.create(path)
+    fsData.write("This is a test to check the block location details".getBytes)
+    fsData.write("This is a test to check the block location details".getBytes)
+    fsData.write("This is a test to check the block location details".getBytes)
+    fsData.write("This is a test to check the block location details".getBytes)
+    fsData.write("This is a test to check the block location details".getBytes)
+
+    fsData.close()
+
+    val status = fs.getFileStatus(path)
+    val locations = fs.getFileBlockLocations(status, 0, 10)
+    assert(locations(0).getLength === 250)
+  }
+
   override def afterAll() = {
     Await.ready(AsyncUtil.executeAsync[system_drop_keyspace_call](client.system_drop_keyspace("FS", _)), 10 seconds)
     clientManager.stop()
