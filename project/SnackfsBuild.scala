c70fbdeccf11bac89bbdd9b7104d0fd012ffd748
Rohit Rai <rohit@tuplejump.com>
2014-03-01 17:09:45 +0530

    Updating build file to reduce to minimum dependencies reducing chances of conflicts

--- project/SnackfsBuild.scala
+++ project/SnackfsBuild.scala
@@ -22,32 +22,105 @@ import sbt.Keys._
 
 object SnackfsBuild extends Build {
 
-  lazy val cas_version = "1.2.12"
+  lazy val VERSION = "0.6-EA"
+
+  lazy val SCALA_VERSION = "2.9.3"
+
+  lazy val SPARK_VERSION = "0.8.1-incubating"
+
+  lazy val CAS_VERSION = "1.2.12"
+
+  lazy val THRIFT_VERSION = "0.7.0"
+
+  lazy val TWITTER_UTIL_VERSION = "2.9.2-6.7.0"
 
   lazy val dist = TaskKey[Unit]("dist", "Generates project distribution")
+
+  lazy val pom = {
+    <scm>
+      <url>git@github.com:tuplejump/snackfs.git</url>
+      <connection>scm:git:git@github.com:tuplejump/snackfs.git</connection>
+    </scm>
+      <developers>
+        <developer>
+          <id>milliondreams</id>
+          <name>Rohit Rai</name>
+          <url>https://twitter.com/milliondreams</url>
+        </developer>
+        <developer>
+          <id>shiti</id>
+          <name>Shiti Saxena</name>
+          <url>https://eraoferrors.blogspot.com</url>
+        </developer>
+      </developers>
+  }
+
+  lazy val dependencies = Seq("org.apache.hadoop" % "hadoop-core" % "1.0.4" % "provided",
+    "org.apache.spark" %% "spark-core" % SPARK_VERSION % "provided",
+    "org.apache.cassandra" % "cassandra-thrift" % CAS_VERSION intransitive(),
+    "org.apache.cassandra" % "cassandra-all" % CAS_VERSION intransitive(),
+    "org.apache.thrift" % "libthrift" % THRIFT_VERSION exclude("org.slf4j", "slf4j-api") exclude("javax.servlet", "servlet-api"),
+    "commons-pool" % "commons-pool" % "1.6",
+    "org.scalatest" %% "scalatest" % "1.9.1" % "it,test",
+    "org.apache.commons" % "commons-io" % "1.3.2" % "it,test",
+    "com.novocode" % "junit-interface" % "0.10" % "it,test",
+    "org.apache.commons" % "commons-lang3" % "3.1" % "it,test",
+    "com.twitter" % "util-logging_2.9.2" % "6.7.0",
+    "log4j" % "log4j" % "1.2.16"
+  )
+
+  lazy val snackSettings = Project.defaultSettings ++ Seq(
+    name := "snackfs",
+
+    organization := "com.tuplejump",
+
+    version := VERSION,
+
+    scalaVersion := SCALA_VERSION,
+
+    parallelExecution in Test := false,
+
+    retrieveManaged := true,
+
+    libraryDependencies ++= dependencies,
+
+    parallelExecution in Test := false,
+
+    pomExtra := pom,
+
+    publishArtifact in Test := false,
+
+    pomIncludeRepository := {
+      _ => false
+    },
+
+    publishMavenStyle := true,
+
+    retrieveManaged := true,
+
+    publishTo <<= version {
+      (v: String) =>
+        val nexus = "https://oss.sonatype.org/"
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
+
+    licenses := Seq("Apache License, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
+
+    homepage := Some(url("https://tuplejump.github.io/calliope/snackfs.html")),
+
+    organizationName := "Tuplejump, Inc.",
+
+    organizationHomepage := Some(url("http://www.tuplejump.com"))
+
+  )
 
   lazy val snackfs = Project(
     id = "snackfs",
     base = file("."),
-    settings = Project.defaultSettings ++ Seq(
-      name := "snackfs",
-      organization := "com.tuplejump",
-      version := "0.5-EA",
-      scalaVersion := "2.9.3",
-      parallelExecution in Test := false,
-      retrieveManaged := true,
-
-      libraryDependencies ++= Seq("org.apache.hadoop" % "hadoop-core" % "1.0.4" % "provided",
-        "org.apache.cassandra" % "cassandra-thrift" % cas_version,
-        "org.apache.cassandra" % "cassandra-all" % cas_version,
-        "commons-pool" % "commons-pool" % "1.6",
-        "org.scalatest" %% "scalatest" % "1.9.1" % "it,test",
-        "org.apache.commons" % "commons-io" % "1.3.2" % "it,test",
-        "com.novocode" % "junit-interface" % "0.10" % "it,test",
-        "org.apache.commons" % "commons-lang3" % "3.1" % "it,test",
-        "com.twitter" % "util-logging_2.9.2" % "6.7.0"
-      )
-    ) ++ Seq(distTask)
+    settings = snackSettings ++ Seq(distTask)
   ).configs(IntegrationTest).settings(Defaults.itSettings: _*)
 
   def distTask = dist in Compile <<= (packageBin in Compile, version in Compile, streams) map {
@@ -61,18 +134,18 @@ object SnackfsBuild extends Build {
       val conf = destination + "conf/"
       val spark = destination + "snack-spark/"
 
-      val forSpark = Set("cassandra-all-" + cas_version + ".jar",
-        "cassandra-thrift-" + cas_version + ".jar",
+      val forSpark = Set("cassandra-all-" + CAS_VERSION + ".jar",
+        "cassandra-thrift-" + CAS_VERSION + ".jar",
         "commons-pool-1.6.jar",
-        "libthrift-0.7.0.jar",
-        "util-core_2.9.2-6.7.0.jar", "util-logging_2.9.2-6.7.0.jar", "snackfs_2.9.3-0.5-EA.jar")
+        "libthrift-" + THRIFT_VERSION + ".jar",
+        "util-core_" + TWITTER_UTIL_VERSION + ".jar", "util-logging_" + TWITTER_UTIL_VERSION + ".jar", "snackfs_" + SCALA_VERSION + "-" + VERSION + ".jar")
 
       IO.copyFile(f, new File(lib + f.getName))
       IO.copyFile(f, new File(spark + f.getName))
 
       /*Dependencies*/
-      IO.copyFile(new File(ivyHome + "org.scala-lang/scala-library/jars/scala-library-2.9.3.jar"),
-        new File(lib + "scala-library-2.9.3.jar"))
+      IO.copyFile(new File(ivyHome + "org.scala-lang/scala-library/jars/scala-library-" + SCALA_VERSION + ".jar"),
+        new File(lib + "scala-library-" + SCALA_VERSION + ".jar"))
 
       val jars = getLibraries
       jars.foreach(j => {
@@ -108,8 +181,8 @@ object SnackfsBuild extends Build {
     val jarSource = "lib_managed/jars/"
 
     val cassandra = jarSource + "org.apache.cassandra/"
-    val cassandraRelated = List(cassandra + "cassandra-all/cassandra-all-" + cas_version + ".jar",
-      cassandra + "cassandra-thrift/cassandra-thrift-" + cas_version + ".jar",
+    val cassandraRelated = List(cassandra + "cassandra-all/cassandra-all-" + CAS_VERSION + ".jar",
+      cassandra + "cassandra-thrift/cassandra-thrift-" + CAS_VERSION + ".jar",
       jarSource + "org.apache.thrift/libthrift/libthrift-0.7.0.jar",
       jarSource + "commons-pool/commons-pool/commons-pool-1.6.jar"
     )
@@ -117,15 +190,15 @@ object SnackfsBuild extends Build {
     val hadoopRelated = List(jarSource + "org.apache.hadoop/hadoop-core/hadoop-core-1.0.4.jar",
       jarSource + "commons-cli/commons-cli/commons-cli-1.2.jar",
       jarSource + "commons-configuration/commons-configuration/commons-configuration-1.6.jar",
-      jarSource + "commons-lang/commons-lang/commons-lang-2.6.jar",
+      jarSource + "commons-lang/commons-lang/commons-lang-2.5.jar",
       jarSource + "commons-logging/commons-logging/commons-logging-1.1.1.jar"
     )
 
     val jackson = jarSource + "org.codehaus.jackson/"
     val log4j = "lib_managed/bundles/log4j/log4j/"
 
-    val otherHadoopDeps = List(jackson + "jackson-core-asl/jackson-core-asl-1.9.2.jar",
-      jackson + "jackson-mapper-asl/jackson-mapper-asl-1.9.2.jar",
+    val otherHadoopDeps = List(jackson + "jackson-core-asl/jackson-core-asl-1.8.8.jar",
+      jackson + "jackson-mapper-asl/jackson-mapper-asl-1.8.8.jar",
       log4j + "log4j-1.2.16.jar",
       jarSource + "org.slf4j/slf4j-log4j12/slf4j-log4j12-1.7.2.jar",
       jarSource + "org.slf4j/slf4j-api/slf4j-api-1.7.2.jar"
